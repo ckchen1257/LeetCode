685cad00adec13356d1b77e5e292df5842889b9c
ckchen1257 <walruschen1257@gmail.com>
2024-05-05 22:13:26 +0800

    Feat：Complete the 55th Problem

---
+++ LeetCode.UnitTest/TopInterview150/55/SolutionTests.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.TopInterview150._55;
+
+namespace LeetCode.UnitTest.TopInterview150._55
+{
+    public class SolutionTests
+    {
+        public static TheoryData<int[], bool> TestCase()
+        {
+            return new TheoryData<int[], bool>
+            {
+                { [2, 3, 1, 1, 4], true },
+                { [3, 2, 1, 0, 4], false },
+                { [0], true },
+                { [2, 2, 0, 0], true }
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(TestCase))]
+        public void CanJump(int[] nums, bool expectedValue)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            var output = solution.CanJump(nums);
+
+            // assert
+            Assert.Equal(expectedValue, output);
+        }
+    }
+}
---
+++ LeetCode/TopInterview150/55/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.TopInterview150._55
+{
+    public class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            int goalIndex = nums.Length - 1;
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                if (i + nums[i] >= goalIndex)
+                {
+                    goalIndex = i;
+                }
+            }
+            return goalIndex == 0;
+        }
+
+        public bool CanJump_1(int[] nums)
+        {
+            int lastIndex = nums.Length - 1;
+
+            bool[] isSuccess = new bool[nums.Length];
+            isSuccess[lastIndex] = true;
+
+            for (int i = lastIndex - 1; i >= 0; i--)
+            {
+                if (i + nums[i] > lastIndex)
+                {
+                    isSuccess[i] = true;
+                }
+                for (int j = 1; j <= nums[i]; j++)
+                {
+                    if (isSuccess[i + j])
+                    {
+                        isSuccess[i] = true;
+                        break;
+                    }
+                }
+            }
+            return isSuccess[0];
+        }
+    }
+}
