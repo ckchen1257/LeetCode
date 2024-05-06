756a7b333738ddb6b4ecb720c55b1a6c6ea47dde
ckchen1257 <walruschen1257@gmail.com>
2024-05-06 23:46:31 +0800

    Feat：Complete the 45th Problem

---
+++ LeetCode.UnitTest/TopInterview150/45/SolutionTests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.TopInterview150._45;
+
+namespace LeetCode.UnitTest.TopInterview150._45
+{
+    public class SolutionTests
+    {
+        public static TheoryData<int[], int> TestCase()
+        {
+            return new TheoryData<int[], int>
+            {
+                { [2, 3, 1, 1, 4], 2 },
+                { [2, 3, 0, 1, 4], 2 },
+                { [0], 0 },
+                { [1], 0 },
+                { [1, 1, 1, 1], 3 },
+                { [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0], 3 }
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(TestCase))]
+        public void Jump(int[] nums, int expectedValue)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            var output = solution.Jump(nums);
+
+            // assert
+            Assert.Equal(expectedValue, output);
+        }
+    }
+}
---
+++ LeetCode/TopInterview150/45/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.TopInterview150._45
+{
+    public class Solution
+    {
+        public int Jump(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                int minSteps = 0;
+                for (int j = 1; j <= nums[i]; j++)
+                {
+                    if (i + j >= nums.Length - 1)
+                    {
+                        minSteps = 1;
+                        continue;
+                    }
+                    if (nums[i + j] == 0)
+                    {
+                        continue;
+                    }
+                    if (minSteps == 0 || minSteps > nums[i + j] + 1)
+                    {
+                        minSteps = nums[i + j] + 1;
+                    }
+                }
+                nums[i] = minSteps;
+            }
+            return nums[0];
+        }
+    }
+}
