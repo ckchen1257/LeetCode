b2720cdd917cbb663b757cfe00e011979185b6de
ckchen1257 <walruschen1257@gmail.com>
2024-05-08 00:24:17 +0800

    Feat：Complete the 274th Problem

---
+++ LeetCode.UnitTest/TopInterview150/274/SolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.TopInterview150._274;
+
+namespace LeetCode.UnitTest.TopInterview150._274
+{
+    public class SolutionTests
+    {
+        public static TheoryData<int[], int> TestCase()
+        {
+            return new TheoryData<int[], int>
+            {
+                { [3, 0, 6, 1 , 5], 3 },
+                { [1, 3, 1], 1 }
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(TestCase))]
+        public void HIndex(int[] nums, int expectedValue)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            var output = solution.HIndex(nums);
+
+            // assert
+            Assert.Equal(expectedValue, output);
+        }
+    }
+}
---
+++ LeetCode/TopInterview150/274/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.TopInterview150._274
+{
+    public class Solution
+    {
+        public int HIndex(int[] nums)
+        {
+            var counts = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int place = nums[i];
+                if (place > nums.Length)
+                {
+                    place = nums.Length;
+                }
+                for (int j = 0; j < place; j++)
+                {
+                    counts[j]++;
+                }
+            }
+            int hIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (counts[i] >= i + 1)
+                {
+                    hIndex = i + 1;
+                }
+            }
+            return hIndex;
+        }
+    }
+}
