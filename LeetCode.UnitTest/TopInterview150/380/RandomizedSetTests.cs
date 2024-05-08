4414813ca3284265b238b192cc5bcf16968c033f
ckchen1257 <walruschen1257@gmail.com>
2024-05-09 00:25:58 +0800

    Feat：Complete the 380th Problem
    1. Solve the Problem
    2. Add some random unitTest for GetRandom()

---
+++ LeetCode.UnitTest/TopInterview150/380/RandomizedSetTests.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.TopInterview150._380;
+
+namespace LeetCode.UnitTest.TopInterview150._380
+{
+    public class RandomizedSetTests
+    {
+        private const double _testSuccessRate = 1.0 / 10000;
+
+        public static TheoryData<(Func<RandomizedSet, bool> Func, bool ExpectedResult)[], HashSet<int>> TestCase()
+        {
+            return new TheoryData<(Func<RandomizedSet, bool> Func, bool ExpectedResult)[], HashSet<int>>()
+            {
+                {
+                    [
+                        ((set) => set.Insert(1), true),
+                        ((set) => set.Remove(2), false),
+                        ((set) => set.Insert(2), true),
+                        ((set) => set.Remove(1), true),
+                        ((set) => set.Insert(1), true),
+                        ((set) => set.Insert(2), false),
+                        ((set) => set.Insert(3), true),
+                        ((set) => set.Insert(4), true),
+                        ((set) => set.Insert(5), true),
+                        ((set) => set.Insert(6), true),
+                        ((set) => set.Insert(7), true),
+                        ((set) => set.Insert(8), true),
+                        ((set) => set.Insert(9), true),
+                        ((set) => set.Insert(10), true)
+                    ],
+                    [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCase))]
+        public void RandomizedSet((Func<RandomizedSet, bool> Func, bool ExpectedResult)[] actionResultPairs, HashSet<int> nums)
+        {
+            // arrange
+            double numCount = nums.Count;
+            double randomTimes = (int)(Math.Log(_testSuccessRate) / Math.Log((numCount - 1) / numCount)) + 1;
+            var randomizedSet = new RandomizedSet();
+
+            // act & assert
+            foreach (var pair in actionResultPairs)
+            {
+                var results = pair.Func(randomizedSet);
+                Assert.Equal(pair.ExpectedResult, results);
+            }
+
+            // assert
+            HashSet<int> randomNums = [];
+            for (int i = 0; i < randomTimes; i++)
+            {
+                randomNums.Add(randomizedSet.GetRandom());
+            }
+            Assert.Equal(nums, randomNums);
+        }
+    }
+}
---
+++ LeetCode/TopInterview150/380/RandomizedSet.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.TopInterview150._380
+{
+    /**
+     * Your RandomizedSet object will be instantiated and called as such:
+     * RandomizedSet obj = new RandomizedSet();
+     * bool param_1 = obj.Insert(val);
+     * bool param_2 = obj.Remove(val);
+     * int param_3 = obj.GetRandom();
+     */
+    public class RandomizedSet
+    {
+        private readonly HashSet<int> _hashSet = [];
+        public RandomizedSet()
+        {
+
+        }
+
+        public bool Insert(int val)
+        {
+            return _hashSet.Add(val);
+        }
+
+        public bool Remove(int val)
+        {
+            return _hashSet.Remove(val);
+        }
+
+        public int GetRandom()
+        {
+            var index = Random.Shared.Next(0, _hashSet.Count);
+            return _hashSet.Skip(index).First();
+        }
+    }
+}
