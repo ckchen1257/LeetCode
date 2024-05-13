1f86e9e4282aee79276af0c9f52c41111d836c8d
ckchen1257 <walruschen1257@gmail.com>
2024-05-14 01:46:52 +0800

    Feat：Complete the 134th Problem

---
+++ LeetCode.UnitTest/TopInterview150/134/SolutionTests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.TopInterview150._134;
+
+namespace LeetCode.UnitTest.TopInterview150._134
+{
+    public class SolutionTests
+    {
+        public static TheoryData<int[], int[], int> TestCase()
+        {
+            return new TheoryData<int[], int[], int>
+            {
+                // -2, -2, -2, 3, 3
+                // -1, -1, 1
+                // 1, -3, 1, -2, 3
+                { [1,2,3,4,5], [3,4,5,1,2], 3 },
+                { [2,3,4], [3,4,3], -1 },
+                { [5,1,2,3,4], [4,4,1,5,1], 4 },
+                { [0,2,0], [1,0,0], 1 },
+                { [1,1,1], [1,2,1], -1 },
+                { [1], [1], 0 },
+                { [1], [2], -1 }
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(TestCase))]
+        public void CanCompleteCircuit(int[] gas, int[] cost, int expectedOutput)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            var output = solution.CanCompleteCircuit(gas, cost);
+
+            // assert
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
---
+++ LeetCode/TopInterview150/134/Solution.cs
@@ -0,0 +1,101 @@
+﻿namespace LeetCode.TopInterview150._134;
+
+public class Solution
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        int nonZeroIndex = 0;
+        for (int i = 0; i < gas.Length; i++)
+        {
+            gas[i] -= cost[i];
+            if (gas[i] != 0)
+            {
+                nonZeroIndex = i;
+            }
+        }
+
+        (int startIndex, int endIndex) = gas[nonZeroIndex] > 0 ?
+            (nonZeroIndex, (nonZeroIndex + 1) % gas.Length) :
+            ((nonZeroIndex + gas.Length - 1) % gas.Length, nonZeroIndex);
+
+        int accumulatedGas = gas[startIndex] + gas[endIndex];
+        while (startIndex != endIndex)
+        {
+            if (accumulatedGas >= 0)
+            {
+                endIndex = endIndex == gas.Length - 1 ? 0 : endIndex + 1;
+                accumulatedGas += gas[endIndex];
+            }
+            else
+            {
+                startIndex = startIndex == 0 ? gas.Length - 1 : startIndex - 1;
+                accumulatedGas += gas[startIndex];
+            }
+        }
+        return accumulatedGas < 0 ? -1 : startIndex;
+    }
+
+    public int CanCompleteCircuit_v1(int[] gas, int[] cost)
+    {
+        for (int i = 0; i < gas.Length; i++)
+        {
+            gas[i] -= cost[i];
+        }
+
+        (int Start, int End) index = (0, 0);
+        for (int i = 0; i < gas.Length; i++)
+        {
+            if (gas[i] == 0)
+            {
+                continue;
+            }
+            bool isPositive = gas[i] > 0;
+            index = isPositive ?
+                (i, (i + 1) % gas.Length) :
+                ((i + gas.Length - 1) % gas.Length, i);
+            break;
+        }
+
+        int accumulatedGas = gas[index.Start] + gas[index.End];
+        while (index.Start != index.End)
+        {
+            if (accumulatedGas >= 0)
+            {
+                index.End = index.End == gas.Length - 1 ? 0 : index.End + 1;
+                accumulatedGas += gas[index.End];
+            }
+            else
+            {
+                index.Start = index.Start == 0 ? gas.Length - 1 : index.Start - 1;
+                accumulatedGas += gas[index.Start];
+            }
+        }
+        return accumulatedGas < 0 ? -1 : index.Start;
+    }
+
+    public int CanCompleteCircuit_TimeLimitExceeded(int[] gas, int[] cost)
+    {
+        for (int i = 0; i < cost.Length; i++)
+        {
+            int accumulatedGas = 0;
+            int accumulatedCost = 0;
+
+            for (int j = 0; j < cost.Length; j++)
+            {
+                int index = (i + j) % cost.Length;
+                accumulatedGas += gas[index];
+                accumulatedCost += cost[index];
+
+                if (accumulatedGas < accumulatedCost)
+                {
+                    break;
+                }
+                if (j == cost.Length - 1)
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
