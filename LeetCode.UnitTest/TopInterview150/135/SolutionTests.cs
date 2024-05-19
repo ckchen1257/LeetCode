7a97eea41bdea70a37171bcf2177a20872cb7ec4
ckchen1257 <walruschen1257@gmail.com>
2024-05-20 00:18:12 +0800

    Feat：Complete the 135th Problem

---
+++ LeetCode.UnitTest/TopInterview150/135/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.TopInterview150._135;
+
+namespace LeetCode.UnitTest.TopInterview150._135
+{
+    public class SolutionTests
+    {
+        public static TheoryData<int[], int> TestCase()
+        {
+            return new TheoryData<int[], int>
+            {
+                { [1,0,2], 5 },
+                { [1,2,2], 4 }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCase))]
+        public void Candy(int[] ratings, int expectedOutput)
+        {
+            // arrange
+            var solution = new Solution();
+
+            // act
+            var output = solution.Candy(ratings);
+
+            // assert
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
---
+++ LeetCode/TopInterview150/135/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.TopInterview150._135;
+
+public class Solution
+{
+    public int Candy(int[] ratings)
+    {
+        if (ratings.Length == 1)
+        {
+            return 1;
+        }
+
+        int[] playerCandies = new int[ratings.Length];
+        int candies = 0;
+
+        for (int i = 0; i < ratings.Length; i++)
+        {
+            SetPlayerCandy(i);
+        }
+        return candies;
+
+        int SetPlayerCandy(int i)
+        {
+            int candy = playerCandies[i];
+            if (candy > 0)
+            {
+                return candy;
+            }
+
+            if (i == 0)
+            {
+                candy = ratings[i] > ratings[i + 1] ? SetPlayerCandy(i + 1) + 1 : 1;
+            }
+            else if (i == ratings.Length - 1)
+            {
+                candy = ratings[i] > ratings[i - 1] ? SetPlayerCandy(i - 1) + 1 : 1;
+            }
+            else
+            {
+                bool isOverPrevious = ratings[i] > ratings[i - 1];
+                bool isOverNext = ratings[i] > ratings[i + 1];
+
+                candy = !isOverPrevious && !isOverNext ? 1 :
+                    isOverPrevious && !isOverNext ? SetPlayerCandy(i - 1) + 1 :
+                    !isOverPrevious && isOverNext ? SetPlayerCandy(i + 1) + 1 :
+                    Math.Max(SetPlayerCandy(i - 1), SetPlayerCandy(i + 1)) + 1;
+            }
+
+            playerCandies[i] = candy;
+            candies += candy;
+            return candy;
+        }
+    }
+}
